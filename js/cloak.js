79a459e35f2c7f6e413b4729d27af8c41ded985e
voxonoly <137123638+voxonoly@users.noreply.github.com>
2024-03-06 22:22:32 -0600

    Website 14

--- js/cloak.js
+++ js/cloak.js
@@ -1,9 +1,6 @@
-fetch("https://irv77.netlify.app/cloak.json")
-  .then((response) => response.json())
-  .then((data) => {
-    const container = document.getElementById("buttons-container");
-
-    data.forEach((link) => {
+fetch("cloak.json").then((response)=>response.json()).then((data)=>{
+  const container = document.getElementById("buttons-container");
+  data.forEach((link)=>{
       const img = document.createElement("img");
       img.className = "presets";
       img.style.width = "50px";
@@ -11,19 +8,18 @@ fetch("https://irv77.netlify.app/cloak.json")
       img.style.borderRadius = "50%";
       img.style.objectFit = "cover";
       img.style.borderColor = "black";
-
       const faviconUrl = `https://www.google.com/s2/favicons?sz=64&domain=${link.favicon}`;
       img.src = link.favicon;
-
-      // Check if the image failed to load, and fallback to link.favicon if it did
-      img.onerror = () => {
-        img.src = faviconUrl;
-      };
-
-      img.addEventListener("click", function () {
-        setTitle(link.title);
-        setFavicon(link.favicon);
+      img.onerror = ()=>{
+          img.src = faviconUrl;
+      }
+      ;
+      img.addEventListener("click", function() {
+          setTitle(link.title);
+          setFavicon(link.favicon);
       });
       container.appendChild(img);
-    });
-  });
+  }
+  );
+}
+);
